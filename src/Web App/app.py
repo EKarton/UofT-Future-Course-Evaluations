5e6d2341fd4e1f7449c36c452acf54a5c3417ff8
Emilio Kartono <emiliokartono@gmail.com>
2020-02-02 21:36:35 -0500

    Added web api for getting past course evals

--- src/Web App/app.py
+++ src/Web App/app.py
@@ -78,21 +78,127 @@ def has_instructor(abbrev_instructor_name):
     return None
 
 def get_fullname_from_abbreviation(course_code, abbrev_instructor_name):
-    dept = course_code[0:3]
 
-    instructor = has_instructor_from_course(course_code, abbrev_instructor_name)
-    if instructor is not None:
-        return instructor
+    if course_code is not None:
+        dept = course_code[0:3]
 
-    instructor = has_instructor_from_dept(dept, abbrev_instructor_name)
-    if instructor is not None:
-        return instructor
+        instructor = has_instructor_from_course(course_code, abbrev_instructor_name)
+        if instructor is not None:
+            return instructor
+
+        instructor = has_instructor_from_dept(dept, abbrev_instructor_name)
+        if instructor is not None:
+            return instructor
 
     instructor = has_instructor(abbrev_instructor_name)
     if instructor is not None:
         return instructor
 
     return None
+
+def get_past_course_ratings_by_course_code(course_code):
+    sql = """SELECT full_instructor_name, term, year, cat_1, cat_2, cat_3, cat_4, cat_5, cat_6, cat_7, cat_8 
+             FROM evaluations 
+             JOIN instructors ON evaluations.instructor_id = instructors.instructor_id 
+             JOIN sessions ON evaluations.session_id = sessions.session_id
+             WHERE course_id = (
+                SELECT course_id
+                FROM courses 
+                WHERE course_code like %s
+             ); """
+
+    results = instructors_table.execute_sql(sql, (course_code, ))
+    parsed_results = []
+    for result in results:
+        parsed_results.append({
+            "instructor": result[0],
+            "term": result[1],
+            "year": result[2],
+            "ratings": result[3 : 11]
+        })
+    
+    return parsed_results
+
+def get_course_ratings_by_instructor(instructor_name):
+    sql = """SELECT course_code, term, year, cat_1, cat_2, cat_3, cat_4, cat_5, cat_6, cat_7, cat_8 
+             FROM evaluations 
+             JOIN sessions ON evaluations.session_id = sessions.session_id 
+             JOIN courses ON evaluations.course_id = courses.course_id 
+             WHERE instructor_id = (
+                SELECT instructor_id 
+                FROM instructors 
+                WHERE instructors.full_instructor_name = %s
+            ); """
+
+    results = instructors_table.execute_sql(sql, (instructor_name, ))
+    parsed_results = []
+    for result in results:
+        parsed_results.append({
+            "course_code": result[0],
+            "term": result[1],
+            "year": result[2],
+            "ratings": result[3 : 11]
+        })
+    
+    return parsed_results
+
+def get_course_ratings_by_course_code_and_instructor(course_code, instructor_name):
+    sql = """SELECT term, year, cat_1, cat_2, cat_3, cat_4, cat_5, cat_6, cat_7, cat_8 
+             FROM evaluations 
+             JOIN sessions ON evaluations.session_id = sessions.session_id 
+             WHERE instructor_id = (
+                 SELECT instructor_id 
+                 FROM instructors 
+                 WHERE instructors.full_instructor_name = %s
+             ) 
+             AND course_id = (
+                 SELECT course_id 
+                 FROM courses 
+                 WHERE courses.course_code = %s
+             ); """
+
+    results = instructors_table.execute_sql(sql, (instructor_name, course_code))
+    parsed_results = []
+    for result in results:
+        parsed_results.append({
+            "term": result[0],
+            "year": result[1],
+            "ratings": result[2 : 10]
+        })
+    
+    return parsed_results
+
+@app.route('/api/v1/evals/past', methods=['GET'])
+def get_past_evals():
+    course_code = None
+    instructor = None
+
+    if 'course_code' in request.args:
+        course_code = request.args.get('course_code')
+
+    if 'instructor' in request.args:
+        instructor = request.args.get('instructor')
+
+    elif 'abbrev_instructor' in request.args:
+        abbrev_instructor = request.args.get('abbrev_instructor')
+        instructor = get_fullname_from_abbreviation(course_code, abbrev_instructor)
+
+        if instructor is None:
+            return 'Abbreviated name ' + abbrev_instructor + ' cannot be known', 400
+
+    print(course_code, instructor)
+
+    if course_code is None and instructor is None:
+        return 'Must have at least \'course_code\', \'instructor\', or \'abbrev_instructor\' in request query', 400
+
+    elif course_code is None:
+        return jsonify(get_course_ratings_by_instructor(instructor)), 200
+
+    elif instructor is None:
+        return jsonify(get_past_course_ratings_by_course_code(course_code)), 200
+
+    else:
+        return jsonify(get_course_ratings_by_course_code_and_instructor(course_code, instructor)), 200
 
 @app.route('/api/evals/future', methods=['GET'])
 def get_future_evals():
